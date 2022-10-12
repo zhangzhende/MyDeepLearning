abb137f668bde5cf120a7a94469cf7eba3b9f73f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-12 12:15:11 +0000

    Adding tarfile member sanitization to extractall()

--- GAN/DiscoGAN/down_resize_crop.py
+++ GAN/DiscoGAN/down_resize_crop.py
@@ -13,11 +13,49 @@ wget.download(shoes_url)
 
 with tarfile.open('./edges2handbags.tar.gz') as tar:
 	"""extractall方法，是把压缩包里面的内容都解压出来，三个参数，path是解压的路径，members是需要解压出来的文件，pwd是密码。"""
-	tar.extractall()
+def is_within_directory(directory, target):
+	
+	abs_directory = os.path.abspath(directory)
+	abs_target = os.path.abspath(target)
+
+	prefix = os.path.commonprefix([abs_directory, abs_target])
+	
+	return prefix == abs_directory
+
+def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+	for member in tar.getmembers():
+		member_path = os.path.join(path, member.name)
+		if not is_within_directory(path, member_path):
+			raise Exception("Attempted Path Traversal in Tar File")
+
+	tar.extractall(path, members, numeric_owner=numeric_owner) 
+	
+
+safe_extract(tar)
 	tar.close()
 
 with tarfile.open('./edges2shoes.tar.gz') as tar:
-	tar.extractall()
+def is_within_directory(directory, target):
+	
+	abs_directory = os.path.abspath(directory)
+	abs_target = os.path.abspath(target)
+
+	prefix = os.path.commonprefix([abs_directory, abs_target])
+	
+	return prefix == abs_directory
+
+def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+	for member in tar.getmembers():
+		member_path = os.path.join(path, member.name)
+		if not is_within_directory(path, member_path):
+			raise Exception("Attempted Path Traversal in Tar File")
+
+	tar.extractall(path, members, numeric_owner=numeric_owner) 
+	
+
+safe_extract(tar)
 	tar.close()
 
 bag_dir = "/edges2handbags/train/"
